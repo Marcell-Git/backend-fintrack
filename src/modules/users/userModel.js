ebd093f9dbcc2ca105236a6b69a8e9de34d1e857
Marcell-Git <220711662@students.uajy.ac.id>
2026-02-11 08:30:56 +0700

    refactore code with prisma

--- src/modules/users/userModel.js
+++ src/modules/users/userModel.js
@@ -1,31 +1,48 @@
-const db = require("../../config/database");
+const prisma = require("../../config/database");
 
 const getAllUsers = async () => {
-  const result = await db.query("SELECT id, username, created_at FROM users");
-  return result.rows;
+  return await prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      created_at: true,
+    },
+  });
 };
 
 const createUser = async (username, password) => {
-  const result = await db.query(
-    "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username, created_at",
-    [username, password],
-  );
-  return result.rows[0];
+  return await prisma.user.create({
+    data: {
+      username,
+      password,
+    },
+    select: {
+      id: true,
+      username: true,
+      created_at: true,
+    },
+  });
 };
 
 const getUserById = async (id) => {
-  const result = await db.query(
-    "SELECT id, username, created_at FROM users WHERE id = $1",
-    [id],
-  );
-  return result.rows[0];
+  return await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+    select: {
+      id: true,
+      username: true,
+      created_at: true,
+    },
+  });
 };
 
 const getUserByUsername = async (username) => {
-  const result = await db.query("SELECT * FROM users WHERE username = $1", [
-    username,
-  ]);
-  return result.rows[0];
+  return await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
 };
 
 module.exports = {
